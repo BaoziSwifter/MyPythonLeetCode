4a233c0e4889e829a0ebf1a766e75a2a0dbde05b
belief <duanlf@knowbox.cn>
2019-11-08 14:39:26 +0800

    easy 2019-11-8

---
+++ 136.只出现一次的数字.py
@@ -0,0 +1,43 @@
+#
+# @lc app=leetcode.cn id=136 lang=python3
+#
+# [136] 只出现一次的数字
+#
+# https://leetcode-cn.com/problems/single-number/description/
+#
+# algorithms
+# Easy (64.02%)
+# Likes:    903
+# Dislikes: 0
+# Total Accepted:    117K
+# Total Submissions: 182.8K
+# Testcase Example:  '[2,2,1]'
+#
+# 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+# 
+# 说明：
+# 
+# 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+# 
+# 示例 1:
+# 
+# 输入: [2,2,1]
+# 输出: 1
+# 
+# 
+# 示例 2:
+# 
+# 输入: [4,1,2,1,2]
+# 输出: 4
+# 
+#
+
+# @lc code=start
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        onceNum = 0
+        for i in nums:
+            onceNum = onceNum ^ i
+        return onceNum
+# @lc code=end
+
