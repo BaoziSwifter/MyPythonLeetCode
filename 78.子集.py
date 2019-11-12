585ae7c34df86c151497a742e872db9f35e85284
belief <duanlf@knowbox.cn>
2019-11-12 15:06:21 +0800

    141 160 198 234 338 46 543 581 78 437

---
+++ 78.子集.py
@@ -0,0 +1,49 @@
+#
+# @lc app=leetcode.cn id=78 lang=python
+#
+# [78] 子集
+#
+# https://leetcode-cn.com/problems/subsets/description/
+#
+# algorithms
+# Medium (75.16%)
+# Likes:    376
+# Dislikes: 0
+# Total Accepted:    44.2K
+# Total Submissions: 58.9K
+# Testcase Example:  '[1,2,3]'
+#
+# 给定一组不含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
+# 
+# 说明：解集不能包含重复的子集。
+# 
+# 示例:
+# 
+# 输入: nums = [1,2,3]
+# 输出:
+# [
+# ⁠ [3],
+# [1],
+# [2],
+# [1,2,3],
+# [1,3],
+# [2,3],
+# [1,2],
+# []
+# ]
+# 
+#
+
+# @lc code=start
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = [[]]
+        for i in nums:
+            res = res + [[i]+item  for item in res ] 
+        return res
+# @lc code=end
+
