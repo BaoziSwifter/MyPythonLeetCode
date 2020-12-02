c89bc17bf68cb5ee011dce98325c87546ce0698f
duanlongfei <feilong@bytedance.com>
2020-12-02 18:40:21 +0800

    26 27 189
    Change-Id: Id6b780e4b45f47923cf38a6a012ca681daa5d545

---
+++ golangLeetcode/27.移除元素.go
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=27 lang=golang
+ *
+ * [27] 移除元素
+ */
+
+// @lc code=start
+func removeElement(nums []int, val int) int {
+	for i := 0; i < len(nums); {
+		if nums[i] == val {
+			nums = append(nums[:i], nums[i+1:]...)
+		} else {
+			i++
+		}
+	}
+	return len(nums)
+}
+
+// @lc code=end
+
