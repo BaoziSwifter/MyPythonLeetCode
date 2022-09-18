918a6175ce6eeee8993ea915ddb7d865d14c3811
beliefduan <beliefduan@tencent.com>
2022-09-18 19:19:55 +0800

    add

--- LoveSwiftLeetcode/239.滑动窗口最大值.swift
+++ LoveSwiftLeetcode/239.滑动窗口最大值.swift
@@ -7,7 +7,30 @@
 // @lc code=start
 class Solution {
     func maxSlidingWindow(_ nums: [Int], _ k: Int) -> [Int] {
-
+        //使用优先队列 和 单调队列的方法，
+        //此处为单调队列的方式,第0个就是最大的值
+        var list = [Int]()
+        var ans = [Int]()
+        var left = 0
+        for right in 0..<nums.count{
+            let len = right - left + 1
+            while !list.isEmpty && list.last! < nums[right]{
+                list.removeLast()
+            }
+            list.append(nums[right])
+            
+            if len < k{
+                continue
+            }
+            ans.append(list[0])
+            //是否左移掉的最大元素
+            if nums[left] == list[0]{
+                //list.removeFirst() 这种写法会超时
+                list = Array(list[1..<list.count])
+            }
+            left += 1
+        }
+        return ans
     }
 }
 // @lc code=end
--- LoveSwiftLeetcode/279.完全平方数.swift
+++ LoveSwiftLeetcode/279.完全平方数.swift
@@ -7,7 +7,17 @@
 // @lc code=start
 class Solution {
     func numSquares(_ n: Int) -> Int {
-
+        var dp = [Int](repeating: 0, count: n + 1)
+        for i in 1...n {
+            var minVal = Int.max
+            var j = 1
+            while j * j <= i {
+                minVal = min(minVal, dp[i - j * j])
+                j += 1
+            }
+            dp[i] = minVal + 1
+        }
+        return dp[n]
     }
 }
 // @lc code=end
